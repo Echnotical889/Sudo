27bd570d468e23b4137d38abc7d97375a4e86f6d
XMagmaCreeperX <35561415+XMagmaCreeperX@users.noreply.github.com>
2019-06-19 21:17:52 +0300

    Add files via upload

---
+++ src/MagmaCreeperYT/Sudo/Main.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace MagmaCreeperYT\Sudo;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat as c;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable(){
+        $this->getLogger()->notice(c::GREEN." Sudo plugin has been enabled.");
+    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
+        if($command->getName() === "sudo"){
+            if(!$sender->hasPermission("sudo.command")){
+                $sender->sendMessage(c::RED."You don't have permission to use this command!");
+                return true;
+            }elseif(count($args) < 2){
+                $sender->sendMessage(c::RED."Usage: /sudo <player> <command | c:message>");
+                return true;
+            }else{
+                $player = $this->getServer()->getPlayer($args[0]);
+                if($player === null){
+                    $sender->sendMessage(c::RED.$args[0]." is not online!");
+                    return true;
+                }else{
+                    if($args[1][0] === "c" && $args[1][1] === ":"){
+                        $message = implode(" ", array_slice($args, 1, 1000));
+                        $message = str_replace("c:", "", $message);
+                        $player->chat($message);
+                        return true;
+                    }else{
+                        $cmd = implode(" ", array_slice($args, 1, 1000));
+                        $this->getServer()->dispatchCommand($player, $cmd);
+                        return true;
+                    }
+                }
+            }
+        }
+    return true;
+    }
+}
